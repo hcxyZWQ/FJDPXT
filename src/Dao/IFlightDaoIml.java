dde93922d22a20186dd23be037fea13046bdf091
54546 <2360703200@qq.com>
2021-01-12 11:57:24 +0800

    创建实现类

---
+++ src/Dao/IFlightDaoIml.java
@@ -0,0 +1,35 @@
+package Dao;
+
+import java.util.Set;
+
+public class IFlightDaoIml implements IFlightDao {
+    @Override
+    public void insertFlight(IFlightDao flight) {
+
+    }
+
+    @Override
+    public Set<IFlightDao> getAllFlight() {
+        return null;
+    }
+
+    @Override
+    public IFlightDao getFlightStartTime(String startTime) {
+        return null;
+    }
+
+    @Override
+    public IFlightDao getFlightStartair(String startair) {
+        return null;
+    }
+
+    @Override
+    public IFlightDao getFlightEndAir(String endair) {
+        return null;
+    }
+
+    @Override
+    public void updateFlight(IFlightDao Flight) {
+
+    }
+}
